996be32b06574a86487f46014e9ca297d1deb121
krxhna <m.krishnakanth1999@gmail.com>
2021-04-04 02:53:29 +0800

    0245

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// import 'dart:js_util';
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    title: 'Your title',
+    home: MyApp(),
+  ));
+}
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              MaterialButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    print("lol");
+                    addUser('hello');
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+Future<void> addUser(nah) {
+  return users
+      .doc(nah)
+      .set({'full_name': "Mary Jane", 'age': 18})
+      .then((value) => print("User Added"))
+      .catchError((error) => print("Failed to add user: $error"));
+}
